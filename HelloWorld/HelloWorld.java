42e7b181827fe10dea9d951cbf46deb4ced492d7
unknown <manurung0071@gmail.com>
2020-09-16 12:00:29 +0700

    HelloWorld.java

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,14 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Nama : Martin Samuel");
+		System.out.println();
+		System.out.println("****************************");
+		System.out.println("Kelas: D3IF4404");
+		System.out.println();
+		System.out.println("****************************");
+		System.out.println("NIM  : 6706204004");
+		System.out.println();
+		System.out.println("****************************");
+	}
+}
